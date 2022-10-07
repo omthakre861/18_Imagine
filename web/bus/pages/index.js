81a0b03bc25beae237a7f35949cd9b6aac5d89d1
Chinmay Dandekar <chinmay2001.cd@gmail.com>
2022-10-07 09:09:26 +0530

    web integration
    Des

---
+++ web/bus/pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import '../styles/Home.module.css'
+import Nav from '../src/components/Nav'
+export default function Home() {
+  return (<>
+    <Nav />
+  </>
+  )
+}
---
+++ web/bus/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+      <div className='nav-main-cont'>
+          <ul className='nav-list'>
+              <li className='nav-items'>a</li>
+              <li className='nav-items'>b</li>
+              <li className='nav-items'>c</li>
+              <li className='nav-items'>d</li>
+          </ul>
+    </div>
+  )
+}
+
+export default Nav
